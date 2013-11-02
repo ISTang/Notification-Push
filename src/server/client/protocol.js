d2509cbeb968e027acd20b3117616355c7228962
ISTang <joebin.don@gmail.com>
2013-11-02 19:03:17 +0800

    优化关注/取消关注事件和消息处理

--- src/server/client/protocol.js
+++ src/server/client/protocol.js
@@ -713,12 +713,14 @@ function handleClientConnection2(socket, appId, accountId, accountName, msgKey, 
                     return callback();
                 }
 
+                if (TRACK_SOCKET) logger.trace("[SOCKET] write to client " + clientAddress + ": Follow public account " + publicAccount + " OK");
+                socket.write(/*PNTP_FLAG+*/FOLLOW_PUBLIC_SUCCESS_RES.format(publicAccount));
+
                 var PUBLIC_ACCOUNT_FOLLOWED_EVENT = JSON.stringify({title: 'FOLLOWED_EVENT', body: accountName, generate_time: new Date()});
                 forwardMsg(appId, accountId, accountName, publicAccount, PUBLIC_ACCOUNT_FOLLOWED_EVENT, "followPublicAccount", function (err) {
 
                     var ss = (err ? "0," + err : "");
                     if (TRACK_SOCKET) logger.trace("[SOCKET] write to client " + clientAddress + ": forward follow event " + (err ? "ERROR" : "OK"));
-                    socket.write(/*PNTP_FLAG+*/FOLLOW_PUBLIC_SUCCESS_RES.format(publicAccount));
                     callback();
                 });
             });
@@ -736,12 +738,14 @@ function handleClientConnection2(socket, appId, accountId, accountName, msgKey, 
                     return callback();
                 }
 
+                if (TRACK_SOCKET) logger.trace("[SOCKET] write to client " + clientAddress + ": Unfollow public account " + publicAccount + " OK");
+                socket.write(/*PNTP_FLAG+*/UNFOLLOW_PUBLIC_SUCCESS_RES.format(publicAccount));
+
                 var PUBLIC_ACCOUNT_UNFOLLOWED_EVENT = JSON.stringify({title: 'UNFOLLOWED_EVENT', body: accountName, generate_time: new Date()});
                 forwardMsg(appId, accountId, accountName, publicAccount, PUBLIC_ACCOUNT_UNFOLLOWED_EVENT, "unfollowPublicAccount", function (err) {
 
                     var ss = (err ? "0," + err : "");
                     if (TRACK_SOCKET) logger.trace("[SOCKET] write to client " + clientAddress + ": forward unfollow event " + (err ? "ERROR" : "OK"));
-                    socket.write(/*PNTP_FLAG+*/UNFOLLOW_PUBLIC_SUCCESS_RES.format(publicAccount));
                     callback();
                 });
             });
